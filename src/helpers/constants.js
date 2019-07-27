a3c26ac131cb86fdee300e28f705cb54961ce192
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-07-27 12:25:55 +0200

    updated BaseUrl

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,5 +1,5 @@
 export const BaseUrl = {
-  login: "http://localhost:5000/api/auth/login",
-  register: "http://localhost:5000/api/register",
-  department: "http://localhost:5000/api/departments/"
+  login: "http://localhost:5000/authentication",
+  register: "http://localhost:5000/users/",
+  department: "http://localhost:5000/departments/"
 };
